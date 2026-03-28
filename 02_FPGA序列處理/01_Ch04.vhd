3a6efe51f2b8bf2e0f2f9a3789af3ab5038772b4
Weiyousang <weiyousang@gmail.com>
2026-03-29 01:06:26 +0800

    Upload project and code

---
+++ 02_FPGA序列處理/01_Ch04.vhd
@@ -0,0 +1,39 @@
+--The IEEE standard 1164 package, declares std_logic, rising_edge(), etc.
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+--
+--*********************************************************
+-- CH04 : practise VECTOR operators
+--                    designed by Pei-Chong Tang, Jan. 1999
+--*********************************************************
+--
+entity CH04 is
+     port
+     (    XB : out STD_LOGIC_VECTOR (7 downto 0);
+          XC : in  STD_LOGIC_VECTOR (3 downto 0)
+     );
+end CH04;
+
+architecture CH04_ARCH of CH04 is
+begin
+--
+--*********************************************************
+-- 3-to-8 address decoding
+--
+     XB <= "00000001" when XC="1000" else
+          "00000010" when XC="1001" else
+          "00000100" when XC="1010" else
+          "00001000" when XC="1011" else
+          "00010000" when XC="1100" else
+          "00100000" when XC="1101" else
+          "01000000" when XC="1110" else
+          "10000000" when XC="1111" else
+          "00000000";
+--
+--*********************************************************
+-- end of architechture
+--
+end CH04_ARCH;
